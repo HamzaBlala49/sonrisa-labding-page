7fd982cc62ec17ea3fe2a4679403a8d7a4d0ab42
HamzaBlala49 <103255525+HamzaBlala49@users.noreply.github.com>
2023-10-24 02:53:05 -0700

    done

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// selectors
+
+
+// global variable
+
+
+// functions
+
+// for owl swiper init
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
